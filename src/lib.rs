ceb9dd1de92b635bdf6e91f8e3c0b7a8fe76a3d9
Andy Gayton <andy@thecablelounge.com>
2023-11-07 13:55:56 -0500

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::sync::{Mutex, MutexGuard};
+use tracing::{info_span, span::Entered};
+use tracing::Span;
+
+// Define your TracingMutex struct here
+pub struct TracingMutex<T> {
+    inner: Mutex<T>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T> TracingMutex<T> {
+    pub fn new(data: T) -> Self {
+        TracingMutex {
+            inner: Mutex::new(data),
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn lock(&self) -> TracingGuard<'_, T> {
+        let span = info_span!("lock_acquired");
+        let enter = span.enter();
+
+        let guard = self.inner.lock().expect("Failed to acquire lock");
+
+        // Store both the span and the Entered guard directly
+        TracingGuard { guard, _enter: enter, _span: span }
+    }
+}
+
+// Updated definition of TracingGuard to include the span
+pub struct TracingGuard<'a, T> {
+    guard: MutexGuard<'a, T>,
+    _enter: Entered<'a>, // This will be the Entered guard, store it directly
+    _span: Span, // Also store the span to extend its lifetime
+}
+
+impl<'a, T> Drop for TracingGuard<'a, T> {
+    fn drop(&mut self) {
+        // The span is exited automatically when _enter is dropped
     }
 }
